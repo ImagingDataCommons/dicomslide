6639871f57d84df81883443c40c4e3d3012f7d74
hackermd <hackermd@protonmail.com>
2022-09-04 17:43:04 -0400

    Update test configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,5 @@ from dicomweb_client import DICOMfileClient
 @pytest.fixture
 def client(tmpdir):
     '''Instance of `dicomweb_client.api.DICOMwebClient`.'''
-    return DICOMfileClient(tmpdir, recreate_db=True)
+    url = f'file://{tmpdir}'
+    return DICOMfileClient(url, recreate_db=True, in_memory=True)
